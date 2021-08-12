1c77b283ca74bdce386e5f68f804eb9258f95e3b
Nam <bangAchieu2@gmail.com>
2021-08-12 20:30:02 +0700

    bai8

---
+++ bai8.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS job_history
+( 
+EMPLOYEE_ID decimal(6,0) NOT NULL, 
+START_DATE date NOT NULL, 
+END_DATE date NOT NULL,
+JOB_ID varchar(10) NOT NULL, 
+DEPARTMENT_ID decimal(4,0) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS job_history
+( 
+EMPLOYEE_ID decimal(6,0) NOT NULL, 
+START_DATE date NOT NULL, 
+END_DATE date NOT NULL,
+JOB_ID varchar(10) NOT NULL, 
+DEPARTMENT_ID decimal(4,0) NOT NULL,
+CHECK (END_DATE = '--/--/----')
+);
