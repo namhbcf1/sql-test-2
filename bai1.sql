8db170dda3a28bf97e0f28f2c3f2bee675c97a8c
Nam <bangAchieu2@gmail.com>
2021-08-12 19:25:17 +0700

    xong

---
+++ bai1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE countries
+( 
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40),
+REGION_ID decimal(10,0)
+);
+
+-- page 7. 2.3. Creating a New Table
+-- page 140. 2.3. Creating a New Table (decimal de viet so thap phan)
