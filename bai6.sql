d1bf099ad9775ec8c27082f1568979f152e157ab
Nam <bangAchieu2@gmail.com>
2021-08-12 20:06:10 +0700

    bai6

---
+++ bai6.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS jobs ( 
+JOB_ID varchar(10) NOT NULL , 
+JOB_TITLE varchar(35) NOT NULL, 
+MIN_SALARY decimal(6,0), 
+MAX_SALARY decimal(6,0),
+CHECK(MAX_SALARY<=25000)
+);
